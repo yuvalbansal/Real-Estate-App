e404aa4d9c22aa34379498703552ddf6a880b3a9
yuvalbansal <yuvalbansal2005.yb@gmail.com>
2025-04-23 17:40:54 +0530

    Fixed Bugs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/Real-Estate-App/' : '',
+  basePath: isProd ? '/Real-Estate-App' : '',
+  output: 'export',
 };
 
 export default nextConfig;
